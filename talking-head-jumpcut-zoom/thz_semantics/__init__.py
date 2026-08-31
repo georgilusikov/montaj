c344a29c373a318aef1c42318f42cad8fa053b59
georgilusikov <georgilusikov@gmail.com>
2026-08-31 16:17:06 +0100

    feat(semantics): export prosody and planner-context helpers

--- talking-head-jumpcut-zoom/thz_semantics/__init__.py
+++ talking-head-jumpcut-zoom/thz_semantics/__init__.py
@@ -1,10 +1,19 @@
+from .context import distance_to_breath_ms, semantic_context_at
 from .probes import detect_salience
+from .prosody import BreathInterval, ProsodyPeak, ProsodySample, detect_breath_intervals, detect_prosody_peaks
 from .schema import ActAnnotation, SalienceHit, SemanticsProvenance, validate_acts
 
 __all__ = [
     "ActAnnotation",
+    "BreathInterval",
+    "ProsodyPeak",
+    "ProsodySample",
     "SalienceHit",
     "SemanticsProvenance",
+    "detect_breath_intervals",
+    "detect_prosody_peaks",
     "detect_salience",
+    "distance_to_breath_ms",
+    "semantic_context_at",
     "validate_acts",
 ]
