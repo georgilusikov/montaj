b3f7c1ad457766352351e8ee1394739c04983811
georgilusikov <georgilusikov@gmail.com>
2026-08-31 16:52:48 +0100

    feat(render): export renderer program provenance hash

--- talking-head-jumpcut-zoom/thz_render/__init__.py
+++ talking-head-jumpcut-zoom/thz_render/__init__.py
@@ -1,6 +1,11 @@
 from .ai_fx import AIDeplasticDrift, default_ai_deplastic_drift, validate_ai_deplastic_drift
 from .contract import RenderKeyframe, RenderSegmentPlan, compile_framing_keyframes, compile_render_plan
-from .ffmpeg import FFmpegSegmentProgram, bind_sendcmd_file, compile_ffmpeg_segment
+from .ffmpeg import (
+    FFmpegSegmentProgram,
+    bind_sendcmd_file,
+    compile_ffmpeg_segment,
+    ffmpeg_program_sha256,
+)
 
 __all__ = [
     "AIDeplasticDrift",
@@ -12,5 +17,6 @@ __all__ = [
     "compile_framing_keyframes",
     "compile_render_plan",
     "default_ai_deplastic_drift",
+    "ffmpeg_program_sha256",
     "validate_ai_deplastic_drift",
 ]
