7f3419c7233fe4b165ba416e3c2cdc0462617e0b
georgilusikov <georgilusikov@gmail.com>
2026-08-31 16:10:32 +0100

    feat(render): add renderer contract package

---
+++ talking-head-jumpcut-zoom/thz_render/__init__.py
@@ -0,0 +1,8 @@
+from .contract import RenderKeyframe, RenderSegmentPlan, compile_framing_keyframes, compile_render_plan
+
+__all__ = [
+    "RenderKeyframe",
+    "RenderSegmentPlan",
+    "compile_framing_keyframes",
+    "compile_render_plan",
+]
